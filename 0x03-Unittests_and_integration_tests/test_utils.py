391ba364c6e9da8d5f33c0385f9ccd6d27e75726
Salsabil <salsabeelahmed323@gmail.com>
2024-08-26 00:01:37 +0300

    3. Parameterize and patch-6

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -69,11 +69,8 @@ class TestMemoize(unittest.TestCase):
 
         # patches the a_method of TestClass so that it returns 42
         # and allows us to track how many times it was called.
-        with patch.object(
-                TestClass,
-                "a_method",
-                return_value=lambda: 42,
-                ) as mock_method:
+        with patch.object(TestClass, 'a_method',
+                          return_value=42) as mock_method:
             test_instance = TestClass()
 
             # Access a_property twice
